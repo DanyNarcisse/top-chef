9d9f7d8635ab012cec7b5869b108ae07f4a727d5
Dany Narcisse <dany.narcisse@devinci.fr>
2018-02-02 16:00:26 +0100

    #2: Adding API url

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,5 +1,6 @@
 //Load HTTP module
 var http = require("http");
+var ulr = "https://restaurant.michelin.fr/search-restaurants?localisation=1424&cooking_type=&gm_selection=&stars=1%7C%7C2%7C%7C3&bib_gourmand=&piecette=&michelin_plate=&services=&ambiance=&booking_activated=&min_price=&max_price=&number_of_offers=&prev_localisation=1424&latitude=&longitude=&bbox_ne_lat=&bbox_ne_lon=&bbox_sw_lat=&bbox_sw_lon=&page_number=0&op=Rechercher&js=true"
 
 //Create HTTP server and listen on port 8000 for requests
 http.createServer(function (request, response) {
@@ -8,7 +9,10 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    // Send the response body "Hello World"
    response.end('Hello World\n');
+    
+    
+    
 }).listen(8080);
 
 // Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/')
+console.log('Server running at http://127.0.0.1:8080/')
