0b3e2b28af10838df1a765a3eb84607e4c857b3a
Dany Narcisse <dany.narcisse@devinci.fr>
2018-02-15 11:11:39 +0100

    New approach, using page url

---
+++ docs/js/michelin.js
@@ -0,0 +1,59 @@
+
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
+
+
+for (var i = 1; i<36; i++){
+    url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-'+String(i);
+    request(url, function(error, response, html){
+        if(!error){
+
+            var $ = cheerio.load(html);
+            var link;
+
+            $('a.poi-card-link').filter(function(){
+                var data = $(this); 
+                link = "https://restaurant.michelin.fr"+data.attr('href');      
+                request(link, function(error, response, body){
+                    var $ = cheerio.load(body);
+                    var json = { title : "", address:"", postCode:""};
+
+
+
+                    $(".poi_intro-display-title").each(function() {
+                        data = $(this);
+                        var title = data.text().trim();
+                        json.title = title;
+
+                    });
+
+                    $(".thoroughfare").each(function() {
+                        data = $(this);
+                        var address = data.text();
+                        json.address = address;
+
+                    });
+
+                    $(".postal-code").each(function() {
+                        data = $(this);
+                        var postCode = data.text();
+                        json.postCode = postCode;
+
+                    });
+
+
+                    fs.appendFile('output.json', JSON.stringify(json)+"\r\n", function(err){
+                        console.log('copy in output.json');
+
+                    });
+
+                });
+
+            })
+
+
+        }
+
+    }) ;}
