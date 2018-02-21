10276157b74637fafd7298887998fd32ba016ee6
DanyNarcisse <dany.narcisse@devinci.fr>
2018-02-21 15:23:23 +0100

    Fixed issue with michelin.js

--- docs/js/michelin.js
+++ docs/js/michelin.js
@@ -1,52 +1,67 @@
+const fetch = require ('node-fetch');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 
 urlPage = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-';
 
+let HRTab = new Array();
 
-for (var i = 1; i<36; i++){
-    url = urlPage + String(i);
-    console.log(url);
-    request(url, function(error, response, html){
-        if(!error){
-
-            var $ = cheerio.load(html);
-            var link;
-
-            $('a.poi-card-link').filter(function(){
-                var data = $(this); 
-                link = "https://restaurant.michelin.fr"+data.attr('href');   
-                console.log(link);
-                request(link, function(error, response, body){
-                    var $ = cheerio.load(body);
-                    var json = { title : "", address:"", postCode:""};
-
-                    $(".poi_intro-display-title").each(function() {
-                        data = $(this);
-                        var title = data.text().trim();
-                        json.title = title;
-                    });
-
-                    $(".thoroughfare").each(function() {
-                        data = $(this);
-                        var address = data.text();
-                        json.address = address;
-                    });
-
-                    $(".postal-code").each(function() {
-                        data = $(this);
-                        var postCode = data.text();
-                        json.postCode = postCode;
-                    });
-
-                    fs.appendFile('data-michelin.json', JSON.stringify(json)+"\r\n", function(err){
-                        console.log('Added to data-michelin.json' + ' page: ' +  String(json.title));
-
-                    });
-                });
-            })
+async function ScrapHRef()
+{
+    let page = 1;
+    let anno = 0;
+    let verif = true;
+    while (verif)
+    {
+        let data = await fetch(urlPage+page.toString());
+        let html = await data.text();
+        let $ = await cheerio.load(html);
+        anno--;
+        if($ ('.srp-no-results-text').text().length > 10)
+        {
+            verif = false;
         }
-    });
-    console.log("It's over");
+        else
+        {
+            $('.poi-card-link').each(
+                function()
+                {
+                    HRTab[page-1+anno]=$(this).attr('href');
+                    console.log(HRTab[page-1+anno]);
+                    anno++;
+                }
+            )
+            page++;
+        }
+    }
+    console.log(HRTab.length);
+    WriteJSON();
 }
+
+async function WriteJSON()
+{
+    var json = { title : "", address:"", postCode:""};
+
+    for(var i=0; i<HRTab.length; i++)
+    {
+        let data = await fetch("https://restaurant.michelin.fr"+HRTab[i]);
+        let html = await data.text();
+        let $ = await cheerio.load(html);
+
+        let title = $(".poi_intro-display-title").text().trim();
+        json.title = title;
+
+        let address = $(".thoroughfare").text().trim();
+        json.address = address;
+
+        let postCode = $(".postal-code").text().trim();
+        json.postCode = postCode;
+
+        fs.appendFile('data-michelin.json', JSON.stringify(json)+"\r\n", function(err){
+            console.log('copy in data-michelin.json' + ' page: ' +  String(json.title));
+        });
+    }
+}
+
+ScrapHRef();
